12520ea92131efbcccd5d739b72b8b13d1371d03
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-06-03 23:55:04 +0530

    UPDATE: test dir moved to api

---
+++ backend/api/test/presenter.routes.test.js
@@ -0,0 +1,28 @@
+const request = require("supertest");
+const Route = require("../routes/presenter.routes");
+
+describe("POST/presenter", () => {
+	test("test save presenter", async () => {
+		const data = {
+			userName: "onella1",
+			firstName: "Onella",
+			lastName: "Natalie",
+			password: "onella1",
+			nic: "9574343443V",
+			qualifications: ["Bsc in SE", "Bsc in IT"],
+			university: "SLIIT",
+			department: "SE",
+		};
+		const respons = await request(Route).post("/create").send(data);
+		expect(respons.statusCode).toBe(200);
+	});
+});
+
+describe("GET/presenter", () => {
+	test("test get all presenters", async () => {
+		const respons = await request(Route).get("/");
+		expect(respons.headers["content-type"]).toEqual(
+			expect.stringContaining("json")
+		);
+	});
+});
