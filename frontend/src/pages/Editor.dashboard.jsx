b30724dac72b1fbf0932bbf9e9e5f69ff82e2537
hasansin <63632399+hasansin@users.noreply.github.com>
2021-06-08 17:57:34 +0530

    Editot_dashboard compenent created

---
+++ frontend/src/pages/Editor.dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import dashboard from "../components/Editor.dashboard/dashboard";
+
+const Editor_dash = () => {
+	return (
+		<div>
+			<Editor_dash />
+		</div>
+	);
+};
+
+export default Editor_dash;
