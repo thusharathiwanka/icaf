b24df8dee4cd9975e64931fd675e51de0bb2fe35
hasansin <63632399+hasansin@users.noreply.github.com>
2021-06-08 17:45:25 +0530

    GetAllNotices implementaion added

--- backend/api/controllers/notice.controller.js
+++ backend/api/controllers/notice.controller.js
@@ -1,2 +1,13 @@
-export const GetAllNotices = (req, res) => {};
+import notice from '../models/notice.model';
+
+export const GetAllNotices = async (req, res) => {
+    try {
+        const notices = notice.find();
+        res.status(200).json(notices);
+        
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+}
+
+};
 export const CreateNotice = (req, res) => {};
