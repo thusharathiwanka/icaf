a53d734d0613f8864ce7a5288d45ea7b3cad9d57
hasansin <63632399+hasansin@users.noreply.github.com>
2021-06-26 12:08:15 +0530

    form and list changed

--- frontend/src/components/Editor.dashboard/AddNotice.jsx
+++ frontend/src/components/Editor.dashboard/AddNotice.jsx
@@ -1,34 +1,67 @@
 import React from 'react';
-
+import { makeStyles } from '@material-ui/core/styles';
 import CloudUploadRoundedIcon from '@material-ui/icons/CloudUploadRounded';
+import TextField from '@material-ui/core/TextField';
 
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(2),
+        width: '50ch',
+        },
+        date: {
+            minWidth: '25ch',
+            float:'right',
+            marginLeft:'30px',
+        },
+
+        
+   
+    },
+  }));
 
 
 
 const AddNotice = () => {
-   
+    const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
+
+    const handleDateChange = (date) => {
+      setSelectedDate(date);
+    };
+
+    const classes = useStyles();
     
-    return (<div className ="AddNotice">
-        <form className="notice_form">
+    return (<div className="AddNotice">
+        
+        <form className={classes.root} >
+    
+        <TextField id="outlined-basic" label="Notice Topic" style={{ backgroundColor: "white" }} variant="outlined" />
+
+        <TextField id="outlined-basic" label="Content" style={{backgroundColor:"white"}}    multiline rows={5}variant="outlined" />
 
             <div >
-                <label className="label_text">Topic: </label>
-                <input className="input_text" type="text" name="topic" placeholder="Add topic"  />
-            </div>
-            <div  >
-                <label className="label_text" style={{ marginTop: "10px" }}>Content: </label>
-                <input className="input_text" style={{ height: "150px" }} type="text" name="Content" placeholder="Add Content" />
-            </div>
-            <div >
-                <label className="label_date"> Date: </label>
-                <input className="input_date" type="date" name="date"/>
-            </div>
-            <div >
-                <p className="label_text">Select a image: <button><CloudUploadRoundedIcon color="action"/></button> </p>
-            </div>
-            <input  className="input_submit" type="submit" value="Submit"/>
+          <div className={classes.date_upload} >
+                   
+                    <MuiPickersUtilsProvider utils={DateFnsUtils} className={ classes.date} style={{backgroundColor: 'white'}}>
+                    <KeyboardDatePicker margin="normal"  label="Date to be post" id="date-picker-dialog" format="MM/dd/yyyy" value={selectedDate} onChange={handleDateChange} KeyboardButtonProps={{  'aria-label': 'change date', }} />
+                    </MuiPickersUtilsProvider>
+                    <p className={ classes.date}>Select a image: <button className="upload"><CloudUploadRoundedIcon color="action" /></button> </p>
+            </div>   
     
-        </form>
+                
+            </div>
+            
+            <button  className="input_submit" type="submit" >Submit</button>
+    
+            </form>
+          
     </div>  );
 }
  
--- frontend/src/pages/Editor.dashboard.jsx
+++ frontend/src/pages/Editor.dashboard.jsx
@@ -25,7 +25,7 @@ const Editor_dash = () => {
 			<div >
 				
 				<NoticeList />
-				<ViewNotice/>
+				
 			</div>
 			
 			
