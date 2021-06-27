9a3143ccb4eb739267b38560c48681888085a367
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-06-27 16:49:59 +0530

    CREATE: notification management added in frontend

--- frontend/src/components/notification/NotificationList.jsx
+++ frontend/src/components/notification/NotificationList.jsx
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import NotificationItem from "./NotificationItem";
+import { getUserType } from "../../auth/userAuth";
+import { BASE_URL } from "../../config/config";
 
 const NotificationList = () => {
+	const [notifications, setNotifications] = useState([]);
+
+	if (getUserType() === "researcher") {
+		fetchEndpoint = "researcher";
+	} else if (getUserType() === "presenter") {
+		fetchEndpoint = "presenter";
+	}
+
+	useEffect(async () => {
+		const res = await fetch(`${BASE_URL}/notification/${fetchEndpoint}`);
+		const data = await res.json();
+		setNotifications(data.notifications);
+	}, []);
+
 	return (
 		<div className="notification-tray">
 			<h2 className="notification-banner">Notifications</h2>
-			<NotificationItem />
-			<NotificationItem />
-			<NotificationItem />
+			{notifications.length > 0 ? (
+				notifications.map((notification) => {
+					return (
+						<NotificationItem
+							notification={notification}
+							setNotifications={setNotifications}
+							notifications={notifications}
+							fetchEndpoint={fetchEndpoint}
+						/>
+					);
+				})
+			) : (
+				<h3 className="light">There are no new notifications</h3>
+			)}
 		</div>
 	);
 };
