4763c87c351e5bf6c014447b054b36d4011d0965
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-06-28 02:35:59 +0530

    CREATE: conference agenda fetching added

--- frontend/src/components/about/Timeline.jsx
+++ frontend/src/components/about/Timeline.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
 import TimelineItem from "./TimelineItem";
+import { BASE_URL } from "../../config/config";
 
 const Timeline = () => {
+	const [agenda, setAgenda] = useState([]);
+
+	useEffect(async () => {
+		const res = await fetch(`${BASE_URL}/conference`);
+		const data = await res.json();
+		setAgenda(data.conference[0].agenda);
+	}, []);
+
 	return (
 		<div className="timeline-content-container">
 			<h1>International Conference on Application Frameworks 2021</h1>
@@ -15,11 +24,9 @@ const Timeline = () => {
 				transition={{ type: "tween", duration: 1 }}
 			>
 				<ul>
-					<TimelineItem />
-					<TimelineItem />
-					<TimelineItem />
-					<TimelineItem />
-					<TimelineItem />
+					{agenda.map((agendaItem) => {
+						return <TimelineItem timelineItem={agendaItem} />;
+					})}
 				</ul>
 			</motion.div>
 		</div>
